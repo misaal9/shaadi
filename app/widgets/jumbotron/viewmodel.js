cbfbf06ef1bf92373f3b42dfb0dd5e9b35d0f862
Rohit Diwakar <misaal9@gmail.com>
2016-10-08 23:01:48 +0530

    Initial commit. WIP.

--- app/widgets/jumbotron/viewmodel.js
+++ app/widgets/jumbotron/viewmodel.js
@@ -1,8 +1,18 @@
 define(function(require){
     'use strict';
-    
+    var ko = require('knockout');
+    var imgSrc = './app/img/pic_';
+
     var JumbotronWidget = function JumbotronWidget(settings) {
         this.settings = settings;
+        var fullImgPath;
+        fullImgPath = imgSrc + this.getRandomImage() + '.jpg';
+        console.info('fullImgPath', fullImgPath);
+        this.randomImgUrl = ko.observable(fullImgPath);
+    };
+    
+    JumbotronWidget.prototype.getRandomImage = function getRandomImage() {
+        return 1;
     };
 
     return JumbotronWidget;
