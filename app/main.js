b7db1f05e742efe251b8f7cd2c183bc17c8891b0
Misaal <misaal9@gmail.com>
2016-10-11 02:32:09 +0530

    Adding night mode bright mode adhoc.

--- app/main.js
+++ app/main.js
@@ -22,7 +22,7 @@ define(function (require) {
         router: true,
         dialog: true,
         widget: {
-            kinds: ['jumbotron', 'thumbnail', 'footer']
+            kinds: ['jumbotron', 'thumbnail', 'footer', 'nightmode']
         }
     });
     
---
+++ app/widgets/nightmode/viewmodel.js
@@ -0,0 +1,43 @@
+define(function(require) {
+    'use strict';
+    var ko = require('knockout'),
+        $ = require('jquery');
+    
+    
+    var availableModes = {
+        night: 'night',
+        bright: 'bright'
+    };
+    
+    var BODY = $('body');
+    
+    var iconClasses = {
+        night: 'fa fa-moon-o fa-3x',
+        bright: 'fa fa-sun-o fa-3x'
+    };
+    
+    var NightModeWidget = function NightModeWidget() {
+        BODY.addClass(availableModes.night);
+        this.isNightMode = ko.observable(true);
+        this.currentModeClass = ko.observable(iconClasses.bright);
+    };
+    
+    NightModeWidget.prototype.changeModeHandler = function changeModeHandler() {
+        if (this.isNightMode()) {
+            this.isNightMode(false);
+        } else {
+            this.isNightMode(true);
+        }
+        this.isNightMode() ? this.currentModeClass(iconClasses.bright) : this.currentModeClass(iconClasses.night);
+        this.updateCss();
+    };
+    
+    NightModeWidget.prototype.updateCss = function updateCss() {
+        var setClassTo = '';
+        BODY.removeClass();
+        setClassTo = this.isNightMode() ?  availableModes.night : availableModes.bright;
+        BODY.addClass(setClassTo);
+    };
+    
+    return NightModeWidget;
+});
